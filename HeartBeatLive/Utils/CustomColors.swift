6cf50abc7e6bb9b4ccb983d6dd87b4edad105acf
Nikita Ivchenko <munoongg@gmail.com>
2022-09-04 15:11:47 +0300

    Split files by folders

---
+++ HeartBeatLive/Utils/CustomColors.swift
@@ -0,0 +1,12 @@
+//
+//  CustomColors.swift
+//  HeartBeatLive
+//
+//  Created by Nikita Ivchenko on 04.09.2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryBackgroundColor: Color = Color(red: 0.158, green: 0.172, blue: 0.203)
+}
