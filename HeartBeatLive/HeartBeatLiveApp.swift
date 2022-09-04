bfdd94d8b50ad5ca4c797ad9bd31bfc175a1ba90
Nikita Ivchenko <munoongg@gmail.com>
2022-09-04 14:46:43 +0300

    Initial Commit

---
+++ HeartBeatLive/HeartBeatLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartBeatLiveApp.swift
+//  HeartBeatLive
+//
+//  Created by Nikita Ivchenko on 04.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HeartBeatLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
